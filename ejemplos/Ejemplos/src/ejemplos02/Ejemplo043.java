f414106b6c2720d834ff1deb4099a08e5d7057e7
DiegoJT22 <daigoestrlar22@gmail.com>
2026-01-07 12:19:19 -0500

    Commit Paquete ejemplos02 Ejemplo043

---
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo043.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos02;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo043 {
+
+    public static void main(String[] args) {
+        // 
+        int[] valores1 = {10,100,1000,20,200,2000};
+        int[] valores2 = {1000,1001,1002,1003,1004,1005};
+        
+        for(int i = 0; i < valores1.length; i++){
+            int v1 = valores1[i];
+            int v2 = valores2[i];
+            obtenerSuma(v1, v2);
+
+        }
+        
+        /*
+        Estamos creando un ciclo for para hacer que se envien
+        los valores al procedimiento obtenerSuma los valores se envian de manera
+        repetitiva a proceso haciendo que funcione el codigo
+        */
+        
+    }
+        
+    public static void obtenerSuma(int a, int b){
+        int suma;
+        suma = a + b;
+        System.out.printf("El valor de la suma es: %d\n", suma);
+        
+    }
+    
+    
+}
